078de06ee9af0f58fbbb054d1190552ca9ca0331
William <yaxingl@usc.edu>
2024-10-15 15:39:21 -0700

    Revert "Commit from the draft"
    This reverts commit c2ac3b65bf9db45efa891a02c4a854308ebce2b1.

--- replit.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }: {
-    deps = [
-        pkgs.imagemagick6_light
-        pkgs.graalvm17-ce
-        pkgs.maven
-        pkgs.replitPackages.jdt-language-server
-        pkgs.replitPackages.java-debug
-    ];
-}
